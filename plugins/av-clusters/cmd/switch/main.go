88eb17158f571b2b8d68944d83a5725f4655716d
sio2boss <sio2boss@hotmail.com>
2022-01-24 17:18:46 -0500

    Better handling of switch arguments

--- plugins/av-clusters/cmd/switch/main.go
+++ plugins/av-clusters/cmd/switch/main.go
@@ -35,9 +35,9 @@ func FindDirectories(root string) ([]string, error) {
 }
 
 type context struct {
-	Account string
-	Env     string
-	Cluster string
+	Account string `docopt:"--account"`
+	Env     string `docopt:"--env"`
+	Cluster string `docopt:"--cluster"`
 }
 
 func main() {
@@ -46,24 +46,21 @@ func main() {
 
 Usage:
   switch
-  switch --env=<env> --account=<env>
+  switch --env=<env> --account=<account> --cluster=<cluster>
   switch -h | --help
   switch --version
 
 Options:
-  --account=<env> Set the selected account manually
-  --env=<env>     Set the environment manually
-  -h --help       Show this screen.
-  --version       Show version.`
+  --account=<account>  Set the selected account manually.
+  --env=<env>          Set the environment manually.
+  --cluster=<cluster>  Set the cluster manually.
+  -h --help            Show this screen.
+  --version            Show version.`
 
 	osargs := os.Args[1:]
 	arguments, _ := docopt.ParseArgs(usage, osargs, "1.0")
 
-	var config struct {
-		environment string `docopt:"--env"`
-		account     string `docopt:"--account"`
-	}
-
+	var config context
 	arguments.Bind(&config)
 
 	// Find cluster directory
@@ -73,90 +70,96 @@ Options:
 	}
 	var clusterdir = string(contents)
 
-	// Walk through clusters
-	accounts, err := FindDirectories(clusterdir)
-	if err != nil {
-		log.Fatalf("Unable to find clusterdir on disk at: %s\n", clusterdir)
-	}
+	var sel_ctx context
+	if config.Account != "" && config.Env != "" && config.Cluster != "" {
+		sel_ctx = config
+	} else {
 
-	// Walk through accounts
-	contexts := []context{}
-	for _, account := range accounts {
+		// Walk through clusters
+		accounts, err := FindDirectories(clusterdir)
+		if err != nil {
+			log.Fatalf("Unable to find clusterdir on disk at: %s\n", clusterdir)
+		}
 
-		// Walk through environments
-		envs, _ := FindDirectories(clusterdir + "/" + account)
-		for _, env := range envs {
+		// Walk through accounts
+		contexts := []context{}
+		for _, account := range accounts {
 
-			// Walk through clusters
-			clusters, _ := FindDirectories(clusterdir + "/" + account + "/" + env)
-			for _, cluster := range clusters {
-				contexts = append(contexts, context{
-					Account: account,
-					Env:     env,
-					Cluster: cluster,
-				})
+			// Walk through environments
+			envs, _ := FindDirectories(clusterdir + "/" + account)
+			for _, env := range envs {
+
+				// Walk through clusters
+				clusters, _ := FindDirectories(clusterdir + "/" + account + "/" + env)
+				for _, cluster := range clusters {
+					contexts = append(contexts, context{
+						Account: account,
+						Env:     env,
+						Cluster: cluster,
+					})
+				}
 			}
 		}
-	}
 
-	// Prompt
-	templates := &promptui.SelectTemplates{
-		Label:    "{{ . }}?",
-		Active:   "▸ {{ .Account | cyan }} \t> {{ .Env | cyan }} \t> {{ .Cluster | cyan }}",
-		Inactive: "  {{ .Account | faint }} \t> {{ .Env | faint }} \t> {{ .Cluster | faint }}",
-		Selected: "▸ {{ .Account | cyan }} > {{ .Env | cyan }} > {{ .Cluster | cyan }}",
-		Details:  "",
-	}
+		// Prompt
+		templates := &promptui.SelectTemplates{
+			Label:    "{{ . }}?",
+			Active:   "▸ {{ .Account | cyan }} \t> {{ .Env | cyan }} \t> {{ .Cluster | cyan }}",
+			Inactive: "  {{ .Account | faint }} \t> {{ .Env | faint }} \t> {{ .Cluster | faint }}",
+			Selected: "▸ {{ .Account | cyan }} > {{ .Env | cyan }} > {{ .Cluster | cyan }}",
+			Details:  "",
+		}
 
-	// TODO provide some details on the selected
-	// `--------- Contexts ----------
-	// {{ "Account:" | faint }}	{{ .Account }}
-	// {{ "Environment:" | faint }}	{{ .Env }}`,
+		// TODO provide some details on the selected
+		// `--------- Contexts ----------
+		// {{ "Account:" | faint }}	{{ .Account }}
+		// {{ "Environment:" | faint }}	{{ .Env }}`,
 
-	searcher := func(input string, index int) bool {
-		context := contexts[index]
-		name := strings.Replace(strings.ToLower(context.Env), " ", "", -1)
-		input = strings.Replace(strings.ToLower(input), " ", "", -1)
-		return strings.Contains(name, input)
-	}
+		searcher := func(input string, index int) bool {
+			context := contexts[index]
+			name := strings.Replace(strings.ToLower(context.Env), " ", "", -1)
+			input = strings.Replace(strings.ToLower(input), " ", "", -1)
+			return strings.Contains(name, input)
+		}
 
-	prompt := promptui.Select{
-		Label:     "Which context would you like to switch to",
-		Items:     contexts,
-		Templates: templates,
-		Size:      len(contexts),
-		Searcher:  searcher,
-	}
+		prompt := promptui.Select{
+			Label:     "Which context would you like to switch to",
+			Items:     contexts,
+			Templates: templates,
+			Size:      len(contexts),
+			Searcher:  searcher,
+		}
 
-	selected, _, err := prompt.Run()
+		selected, _, err := prompt.Run()
 
-	if err != nil {
-		fmt.Printf("Prompt failed %v\n", err)
-		return
+		if err != nil {
+			fmt.Printf("Prompt failed %v\n", err)
+			return
+		}
+		sel_ctx = contexts[selected]
 	}
 
 	// Set pvs
-
 	// Set account
-	cmd := exec.Command("setpv", "account", contexts[selected].Account)
+	cmd := exec.Command("setpv", "account", sel_ctx.Account)
 	if err := cmd.Run(); err != nil {
 		log.Fatalf("Unable to set persistant variable: 'account'\n")
 	}
-	fmt.Println(" - Updated 'account' variable:" + contexts[selected].Account)
+	fmt.Println(" - Updated 'account' variable: " + sel_ctx.Account)
 
 	// Set environment
-	cmd = exec.Command("setpv", "environment", contexts[selected].Env)
+	cmd = exec.Command("setpv", "environment", sel_ctx.Env)
 	if err := cmd.Run(); err != nil {
 		log.Fatalf("Unable to set persistant variable: 'environment'\n")
 	}
-	fmt.Println(" - Updated 'environment' variable:" + contexts[selected].Env)
+	fmt.Println(" - Updated 'environment' variable: " + sel_ctx.Env)
 
 	// Set cluster
-	cmd = exec.Command("setpv", "cluster", contexts[selected].Cluster)
+	cmd = exec.Command("setpv", "cluster", sel_ctx.Cluster)
 	if err := cmd.Run(); err != nil {
 		log.Fatalf("Unable to set persistant variable: 'cluster'\n")
 	}
-	fmt.Println(" - Updated 'cluster' variable:" + contexts[selected].Cluster)
+	fmt.Println(" - Updated 'cluster' variable: " + sel_ctx.Cluster)
 
 	// Symlink .env if available
 	dest, err := os.Readlink(".env")
@@ -164,14 +167,14 @@ Options:
 		fmt.Println(" - Removing existing .env")
 		os.Remove(".env")
 	}
-	err = os.Symlink(".env."+contexts[selected].Env, ".env")
+	err = os.Symlink(".env."+sel_ctx.Env, ".env")
 	if err != nil {
 		log.Fatalf("Unable to update .env symlnk\n%v", err)
 	}
 	fmt.Println(" - Symlinked .env to selected environment")
 
 	// Run persistant variable updates based on selection
-	variables := clusterdir + "/" + contexts[selected].Account + "/" + contexts[selected].Env + "/" + contexts[selected].Cluster + "/inventory/variables.sh"
+	variables := clusterdir + "/" + sel_ctx.Account + "/" + sel_ctx.Env + "/" + sel_ctx.Cluster + "/inventory/variables.sh"
 	if exists(variables) {
 		os.Chmod(variables, 0700)
 		cmd := exec.Command(variables)
@@ -181,7 +184,7 @@ Options:
 		fmt.Println(" - Updated variables as defined in 'inventory/variables.sh'")
 	}
 
-	contextLabel := contexts[selected].Account + " > " + contexts[selected].Env + " > " + contexts[selected].Cluster
+	contextLabel := sel_ctx.Account + " > " + sel_ctx.Env + " > " + sel_ctx.Cluster
 	fmt.Println()
 	fmt.Println("Using", contextLabel, "as environment, you will need to restart your shell")
 
